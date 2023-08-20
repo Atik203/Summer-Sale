cb13291f90ce47a9862fd1edbceb9e204b9de718
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-08-20 20:01:36 +0600

    added go home button

--- js/Add_area.js
+++ js/Add_area.js
@@ -5,7 +5,7 @@ let coupon = false;
 function updateOrder() {
   let orderList = "<ol class='space-y-3'>";
   total = 0;
-
+  // calculate total amount and add name to order list
   for (let i = 0; i < itemList.length; i++) {
     const item = itemList[i];
     orderList += `<li>${i + 1}. ${item.name}</li>`;
@@ -13,13 +13,14 @@ function updateOrder() {
   }
 
   orderList += "</ol>";
-
+  // calculate discount
   let discount = 0;
   if (coupon == true) {
     discount = (total * 20) / 100;
   }
   const discountedTotal = total - discount;
 
+  // add innerHTML in add area
   document.getElementById("add-area").innerHTML = `
    <div class="space-y-2 mb-8"> 
       ${orderList}
@@ -36,22 +37,28 @@ function updateOrder() {
       )} TK</span></p>
     </div>
   `;
-
+  // apply button enabled or disabled
   const applyButton = document.getElementById("apply-btn");
   if (total >= 200) {
     applyButton.removeAttribute("disabled");
   } else {
     applyButton.setAttribute("disabled", "true");
   }
-
+  // clear the shoppings list
   const purchaseButton = document.getElementById("purchase-btn");
+  const clear = document.getElementById("clear-cart");
   if (itemList.length > 0) {
     purchaseButton.style.display = "block";
+    clear.addEventListener("click", function () {
+      my_modal_1.showModal();
+      itemList.length = 0;
+      updateOrder();
+    });
   } else {
     purchaseButton.style.display = "none";
   }
 }
-
+// apply coupon
 function applyCoupon() {
   const couponInput = document.getElementById("coupon-input").value;
   if (couponInput === "SELL200") {
--- js/add_price.js
+++ js/add_price.js
@@ -3,12 +3,11 @@ function InputPrice(inputId) {
   const price = parseFloat(PriceInput);
   return price;
 }
-
+// receive nameID and priceID from card
 function cardClicked(nameId, priceId) {
   const name = document.getElementById(nameId).innerText;
   const priceText = document.getElementById(priceId).innerText;
   const price = parseFloat(priceText);
-
   itemList.push({ name: name, price: price });
   updateOrder();
 }
